31a6d5e597c2798c8d41e46021e71ec88ea63614
resaba.kaypee <resaba.kaypee@gmail.com>
2019-03-30 21:05:11 +0700

    updated html and js

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 let alarmSound = new Audio();
-    alarmSound.src = 'alarm.mp3';
+alarmSound.src = 'alarm.mp3';
+let alarmTimer;
 
-function setAlarm(){
+function setAlarm(button) {
   let ms = document.getElementById('alarmTime').valueAsNumber;
-  if(isNaN(ms)){
+  if (isNaN(ms)) {
     alert("Invalid Date");
     return;
   }
   let alarm = new Date(ms);
-  let alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHour(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
+  let alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
   let differenceInMs = alarmTime.getTime() - (new Date()).getTime();
 
-  if(differenceInMs < 0){
+  if (differenceInMs < 0) {
     alert("Specified time is already passed!");
     return;
   }
 
-  setTimeOut(initAlarm, differenceInMs);
+  alarmTimer = setTimeout(initAlarm, differenceInMs);
+
+  button.innerHTML = "Cancel Alarm";
+  button.setAttribute('onclick', 'cancelAlarm(this)');
 }
 
-function initAlarm(){
+function cancelAlarm(button) {
+  button.innerHTML = "Set Alarm";
+  button.setAttribute('onclick', 'setAlarm(this)');
+  clearTimeout(alarmTimer);
+}
+
+function initAlarm() {
   alarmSound.play();
+  document.getElementById('alarmOptions').style.display = "";
+}
+
+function stopAlarm() {
+  alarmSound.pause();
+  alarmSound.currentTime = 0;
+  document.getElementById('alarmOptions').style.display = 'none';
+  cancelAlarm(document.getElementById('alarmButton'));
+}
+
+function snooze() {
+  stopAlarm();
+  setTimeout(initAlarm, 36000);
 }
